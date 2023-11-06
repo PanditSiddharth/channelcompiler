2e57ba503bce333c19ae5c1b892aa820cb2b990b
PanditSiddharth <75090218+PanditSiddharth@users.noreply.github.com>
2023-11-06 19:03:49 +0000

    Initial commit

---
+++ btdata.ts
@@ -0,0 +1,74 @@
+import config from "./config"
+export let version = `𝐕𝐞𝐫𝐬𝐢𝐨𝐧: ${config.version}\n𝐕𝐞𝐫𝐬𝐢𝐨𝐧 𝐧𝐨.: ${config.versionNo}`
+
+
+export let hcmp = `=========================
+Compilation commands
+=========================
+Add this bot in your channel 
+Now add this any compiler command and your post text after it
+
+Example:
+${config.postSymbol}js console.log('hi')
+
+${config.postSymbol}cc for c
+${config.postSymbol}py python
+${config.postSymbol}js node
+${config.postSymbol}cpp cplus
+${config.postSymbol}jv java
+${config.postSymbol}go for golang
+${config.postSymbol}ts for typescript`
+
+export let hAdmin = `=========================
+Bot admin commands
+=========================
+
+${config.startSymbol}chats in which bot joined
+${config.startSymbol}count chats count
+${config.startSymbol}inf info of chat id with link
+${config.startSymbol}sendTask broadcast message
+${config.startSymbol}sendto send to any chat by bot
+`
+export let hUtil = `=========================
+Control and info commands
+=========================
+
+${config.startSymbol}leave to stop excecution
+${config.startSymbol}ping to see bot's running status
+${config.startSymbol}version to see version and features
+${config.startSymbol}start for basic info
+@help for this help list
+
+${config.channel + " " + config.group}
+${config.owner ? "Owner: " + config.owner : "𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: @PanditSiddharth"}
+`
+export let jUtil = {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Admin", callback_data: JSON.stringify({ ok: "help", action: "admin" }) },
+          { text: "Compiler", callback_data: JSON.stringify({ ok: "help", action: "cmp" }) }
+          ]
+        ]
+      }
+    }
+
+export let jAdmin = {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Utility", callback_data: JSON.stringify({ ok: "help", action: "util" }) },
+          { text: "Compiler", callback_data: JSON.stringify({ ok: "help", action: "cmp" }) }
+          ]
+        ]
+      }
+    }
+
+export let jcmp = {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Utility", callback_data: JSON.stringify({ ok: "help", action: "util" }) },
+          { text: "Admin", callback_data: JSON.stringify({ ok: "help", action: "admin" }) },
+          { text: "Close", callback_data: JSON.stringify({ ok: "help", action: "close" }) }
+          ]
+        ]
+      }
+    }
---
+++ src/addButton.ts
@@ -0,0 +1,93 @@
+import { Telegraf, Context } from "telegraf"
+
+let addButton = (bot: Telegraf) => {
+  bot.on("channel_post", async (ctx: Context) => {
+    try {
+      let cp: any = ctx.channelPost
+
+      let a = (m: any) => {
+        if (cp.text.startsWith(m)) {
+          cp.text = cp.text.replace(m, "")
+          return true
+        }
+        return false
+      }
+
+      let cmp: string = "";
+      let cmpl: string = "";
+
+      if (a("/js"))
+        cmp = "js"
+      else if (a("/py")) {
+        cmp = "py"
+        cmpl = "python"
+      }
+      else if (a("/jv")) {
+        cmp = "jv"
+        cmpl = "java"
+      }
+      else if (a("/cc")) {
+        cmp = "code";
+        cmpl = "c";
+      }
+      else if (a("/cpp"))
+        cmp = "cpp"
+      else if (a("/go"))
+        cmp = "go"
+      else if (a("/ts"))
+        cmp = "ts"
+
+      if (!cmp)
+        return
+      else if (!cmpl)
+        cmpl = cmp
+
+      ctx.deleteMessage().catch((err: any) => { })
+
+      let firstLine: any = cp.text.trim().split('\n')[0].trim();
+      if (firstLine.match(/^(\/\/|#)/)) {
+        cp.text = cp.text.trim().replace(firstLine, "")
+      }
+
+      cp.text = cp.text.replace(/([\s\S]*)/m, (match: any, first: any) => {
+
+        return "```" + cmpl + "\n" + first.trim() + "```"
+      }).replace(/(\\)/g, "\\\\")
+
+      firstLine = firstLine
+.replace(/(\\)/g, "\\\\")
+.replace(/[_*[\]()~`>#+=|{}.!]/g, '\\$&');
+
+      if (firstLine.match(/^(\/\/|#)/))
+        cp.text = "**" + firstLine + "**\n" + cp.text.trim();
+      
+
+
+      if (cmp == "py") {
+        ctx.reply(cp.text, {
+          reply_markup: {
+            inline_keyboard: [
+              [{ text: "Run", callback_data: "" + cmp },
+              { text: "show", url: "t.me/python_0bot?start=run" }]
+            ]
+          },
+          parse_mode: "MarkdownV2"
+        }).catch((err: any) => { console.log(err) })
+
+      } else {
+        ctx.reply(cp.text, {
+          reply_markup: {
+            inline_keyboard: [
+              [{ text: "Run", callback_data: "" + cmp },
+              { text: "show", url: "t.me/compiler0bot?start=run" }]
+            ]
+          },
+          parse_mode: "MarkdownV2"
+        }).catch((err: any) => { console.log(err) })
+      }
+    } catch (error: any) { }
+
+  })
+}
+
+export default addButton;
